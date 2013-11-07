b7df486faa18e08972babe8364c29ee2223fddc1
Caleb James DeLisle <cjd@hyperboria.ca>
2013-11-07 15:29:03 +0100

    Switched to using renderjs-stub.js

--- src/gadget.js
+++ src/gadget.js
@@ -1,24 +1,21 @@
-/*global window, jQuery, rJS */
-(function (window, $, rJS) {
+/*global window, rJS */
+(function () {
   "use strict";
 
-console.log('hello world');
-try{
-console.log(rJS(window));
+  rJS(window).ready(function () {
+    rJS(window).declareMethod('setContent', function (value) {
+      return rJS(this).context.find('textarea').val(value);
 
-  rJS(window).declareMethod('setContent', function (value) {
-    return rJS(this).context.find('textarea').val(value);
+    }).declareMethod('getContent', function () {
+      return rJS(this).context.find('textarea').val();
 
-  }).declareMethod('getContent', function () {
-    return rJS(this).context.find('textarea').val();
-
-  }).declareMethod('setReadOnly', function (isReadOnly) {
-    if (isReadOnly) {
-      rJS(this).context.find('textarea').attr('readonly', 'readonly');
-    } else {
-      rJS(this).context.find('textarea').removeAtte('readonly');
-    }
+    }).declareMethod('setReadOnly', function (isReadOnly) {
+      if (isReadOnly) {
+        rJS(this).context.find('textarea').attr('readonly', 'readonly');
+      } else {
+        rJS(this).context.find('textarea').removeAtte('readonly');
+      }
+    });
   });
-}catch (e) { console.log(e.stack); }
 
-}(window, jQuery, rJS))
+}());
---
+++ src/renderjs-stub.js
@@ -0,0 +1,55 @@
+/* -*- Mode: js; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*! webviewer-stub v0.1  */
+/*global window, document */
+;(function (window, document) {
+  "use strict";
+  if (window.self === window.top) {
+    return;
+  }
+  var readyList = [];
+  var cookie = ''+Math.random();
+  var onMessage = function (event) {
+    var data;
+    try { data = JSON.parse(event.data); } catch (e) { }
+    if (!data || data.cookie !== cookie || data.id !== 'renderjs-stub-response') {
+      return;
+    }
+    if (!cookie) {
+      console.log("got duplicate messages from parent");
+      return;
+    }
+    var scripts = data.scripts;
+    var head = document.getElementsByTagName('head')[0];
+    var scr;
+    for (var i = 0; i < scripts.length; i++) {
+      scr = document.createElement('script');
+      scr.setAttribute('src', data.scripts[i]);
+      scr.setAttribute('type', 'text/javascript');
+      head.appendChild(scr);
+    }
+    if (!scr) {
+      throw new Error("RenderJS not provided");
+    }
+    scr.onload = function () {
+      var root = window.renderJS(window);
+      if (root.ready === readyList.push) {
+        throw new Error("RenderJS not loaded");
+      }
+      for (var i = 0; i < readyList.length; i++) {
+        root.ready(readyList[i]);
+        //readyList[i](root);
+      }
+    }
+  }
+  if (window.addEventListener) {
+    window.addEventListener('message', onMessage);
+  } else {
+    window.attachEvent('onmessage', onMessage);
+  }
+  window.top.postMessage({cookie:cookie, id:"renderjs-stub-request"}, '*');
+
+  window.renderJS = window.rJS = function (window) {
+    return { ready: function (func) { readyList.push(func); } };
+  };
+
+}(window, document));
